f296b7b5e7911c75a6e2d1ddc35b7fa64bcca73f
Enno Richter <enno@nerdworks.de>
2025-05-19 15:32:46 +0200

    feat: Add autocmd to detect helm files

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,26 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Automatically set filetype to 'helm' if Chart.yaml is found in a parent directory
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("helm_filetype", { clear = true }),
+  pattern = "*", -- Apply to all files
+  callback = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local filename = vim.api.nvim_buf_get_name(bufnr)
+
+    -- Skip if the buffer is not associated with a file
+    if filename == "" or vim.fn.empty(filename) == 1 then
+      return
+    end
+
+    -- Find Chart.yaml in the current directory or any parent directory
+    local chart_yaml_path = vim.fn.findfile("Chart.yaml", vim.fn.expand("%:h") .. ";", -1)
+
+    -- If Chart.yaml is found, set the filetype to helm
+    if chart_yaml_path ~= "" then
+      vim.bo[bufnr].filetype = "helm"
+    end
+  end,
+})
