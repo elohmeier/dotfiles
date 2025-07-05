0bd4e068845105d0c010dc72f9724ae1d46995f5
Enno Richter <enno@nerdworks.de>
2025-07-05 05:30:52 +0200

    use higher contrast theme

--- home/dot_config/nvim/lua/plugins/tokyonight.lua
+++ home/dot_config/nvim/lua/plugins/tokyonight.lua
@@ -1,12 +1,14 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    style = "day",
-  },
-  config = function()
-    -- Load the colorscheme
-    vim.cmd([[colorscheme tokyonight]])
-  end,
-}
+return {}
+
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = {
+--     style = "day",
+--   },
+--   config = function()
+--     -- Load the colorscheme
+--     vim.cmd([[colorscheme tokyonight]])
+--   end,
+-- }
