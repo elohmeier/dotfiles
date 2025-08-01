e14f38a89786d84125b47e3be415652ec2108ed6
Enno Richter <enno@nerdworks.de>
2025-08-01 07:00:10 +0200

    feat(nvim): configure jupytext

--- home/dot_config/nvim/lua/plugins/gitsigns.lua
+++ home/dot_config/nvim/lua/plugins/gitsigns.lua
@@ -3,6 +3,13 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   opts = {
     current_line_blame = false,
+    on_attach = function(bufnr)
+      if vim.api.nvim_buf_get_name(bufnr):match('%.ipynb$') then
+        -- Do not attach for .ipynb file, since these are converted
+        -- with jupytext.nvim
+        return false
+      end
+    end,
   },
   keys = {
     -- Leader menu for hunks
