b826765ab2320fed9d518969b0f20a1d9a17ff48
Enno Richter <enno@nerdworks.de>
2025-10-14 09:32:41 +0200

    fix(nvim): optimize resession session saving for large repos

--- home/dot_config/nvim/lua/plugins/resession.lua
+++ home/dot_config/nvim/lua/plugins/resession.lua
@@ -9,6 +9,41 @@ return {
         enabled = true,
         notify = false,
       },
+      -- Minimal options to save - reduces serialization time
+      options = {
+        "binary",
+        "bufhidden",
+        "buflisted",
+        "filetype",
+        "modifiable",
+        "readonly",
+      },
+      -- Disable all extensions (including quickfix) for faster saves
+      extensions = {},
+      -- Filter out special buffers that shouldn't be saved
+      buf_filter = function(bufnr)
+        local buftype = vim.bo[bufnr].buftype
+        local filetype = vim.bo[bufnr].filetype
+
+        -- Exclude special buffer types
+        if buftype ~= "" and buftype ~= "acwrite" then
+          return false
+        end
+
+        -- Exclude neo-tree and other plugin buffers
+        if
+          filetype == "neo-tree"
+          or filetype == "neo-tree-popup"
+          or filetype == "TelescopePrompt"
+          or filetype == "lazy"
+          or filetype == "toggleterm"
+          or filetype == "help"
+        then
+          return false
+        end
+
+        return true
+      end,
     })
 
     -- Automatically save a session when you exit Neovim
